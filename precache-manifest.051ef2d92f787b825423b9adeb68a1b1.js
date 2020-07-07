645f3e75dbe187784174ae21855aeaa8a08e13fb
akshayakky <akshaysmpt01@gmail.com>
2020-07-07 21:24:16 +0530

    Updates

---
+++ precache-manifest.051ef2d92f787b825423b9adeb68a1b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d38f696ee6982bc0330e1349189d5e",
+    "url": "/book-store-frontend/index.html"
+  },
+  {
+    "revision": "8733754c6a1e9d3e3a8d",
+    "url": "/book-store-frontend/static/css/main.8917f35e.chunk.css"
+  },
+  {
+    "revision": "e9149f2c086573d922e9",
+    "url": "/book-store-frontend/static/js/2.d440dbef.chunk.js"
+  },
+  {
+    "revision": "9f9f1911691b9583d02cd314eccd1cd4",
+    "url": "/book-store-frontend/static/js/2.d440dbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8733754c6a1e9d3e3a8d",
+    "url": "/book-store-frontend/static/js/main.112737aa.chunk.js"
+  },
+  {
+    "revision": "ce2f96a70df620dc3393",
+    "url": "/book-store-frontend/static/js/runtime-main.0a9a0308.js"
+  },
+  {
+    "revision": "ccb5f5aa7410f72daea146da37692d4a",
+    "url": "/book-store-frontend/static/media/cracker3.ccb5f5aa.png"
+  }
+]);
